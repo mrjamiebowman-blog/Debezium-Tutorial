cf28c37bac7c19bcf47d560625d4ca3cb14d5011
mrjamiebowman <mrjamiebowman@gmail.com>
2022-11-01 05:00:21 -0500

    .net project

---
+++ src/MrJb.Debezium.Consumer/Program.cs
@@ -0,0 +1,10 @@
+using MrJb.Debezium.Consumer;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+host.Run();
